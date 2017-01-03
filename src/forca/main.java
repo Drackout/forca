f28e5936839eccb609981e83b35b60226dd2a781
Drackout <mikatx2@hotmail.com>
2017-01-03 14:07:39 +0000

    main

--- src/forca/main.java
+++ src/forca/main.java
@@ -3,7 +3,15 @@ package forca;
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Yellowasaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+		System.out.println("Jogo da forca");
+		
+		String[] nomes;
+		nomes = new String[]{ "um", "dois", "tres", "coisa", 
+				"etc", "yolo", "bananas", "repolhos" };
+		
+		
+		
+		
 
 	}
 
