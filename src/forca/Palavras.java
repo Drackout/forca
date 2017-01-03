0e39e7b950e49250544442dc762ffec7ab2fc169
Drackout <mikatx2@hotmail.com>
2017-01-03 15:28:15 +0000

    blep

--- src/forca/Palavras.java
+++ src/forca/Palavras.java
@@ -1,10 +1,21 @@
 package forca;
 
+import java.util.Random;
+
 public class Palavras {
 
 		private String[] nomes = new String[]{ "um", "dois", "tres", "coisa", 
-				"etc", "yolo", "bananas", "repolhos" };
+				"etc", "yolo", "bananas", "repolhos", "manteiga", "olicos" };
 
+		Random gerador = new Random();
+		
+		
+		public Palavras(){
+			int val = gerador.nextInt(10);
+			System.out.println(nomes[val]);
+		}
+		
+		
 		public String[] getNomes() {
 			return nomes;
 		}
--- src/forca/main.java
+++ src/forca/main.java
@@ -1,22 +1,32 @@
 package forca;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class main {
 
 	public static void main(String[] args) {
-		System.out.println("Jogo da forca");
+		Scanner input = new Scanner(System.in);
 		
+		/*
 		Random gerador = new Random();
 		
 		String[] nomes = new String[]{ "um", "dois", "tres", "coisa", 
 				"etc", "yolo", "bananas", "repolhos", "manteiga", "olicos" };
 		
-		
 		int val = gerador.nextInt(10);
-		
-		
 		System.out.println(nomes[val]);
+		*/
+		Palavras teste;
+		
+		System.out.println("Jogo da forca");
+		System.out.print("Palavra gerada: ");
+
+		teste = new Palavras();
+		
+		System.out.print("\n\nLetra: ");
+		String x = input.nextLine();
+		
 		
 		
 		
