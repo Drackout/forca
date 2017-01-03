195684fa2986c2dac1b0cc65b5dff89ae7d637b3
Drackout <mikatx2@hotmail.com>
2017-01-03 14:14:05 +0000

    derp

--- src/forca/main.java
+++ src/forca/main.java
@@ -5,9 +5,6 @@ public class main {
 	public static void main(String[] args) {
 		System.out.println("Jogo da forca");
 		
-		String[] nomes;
-		nomes = new String[]{ "um", "dois", "tres", "coisa", 
-				"etc", "yolo", "bananas", "repolhos" };
 		
 		
 		
