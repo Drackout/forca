a69770b858d4ac5c5db46828eba6cd0ff08c8929
Luís <luis.msandrade@hotmail.com>
2017-01-05 12:13:21 +0000

    grafic

--- src/forca/grafic.java
+++ src/forca/grafic.java
@@ -2,6 +2,67 @@ package forca;
 
 public class grafic {
 
-	
+	public grafic(int erro){
+		if (erro == 0) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        } else if (erro == 1) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        } else if (erro == 2) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#   |");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        } else if (erro == 3) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#  -|");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        }
+        else if (erro == 4) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#  -|-");
+        	System.out.println("#    ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        }
+        else if (erro == 5) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#  -|-");
+        	System.out.println("#  /  ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        }
+        else if (erro == 6) {
+        	System.out.println("\n#####");
+        	System.out.println("#   #");
+        	System.out.println("#   O");
+        	System.out.println("#  -|-");
+        	System.out.println("#  / \\ ");
+        	System.out.println("#    ");
+        	System.out.println("######");
+        }
+	}
 
 }
