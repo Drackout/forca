8e5e47ef81c9d2cd93f373ea31ba0696b3618377
ddias96 <diogo_dias96@hotmail.com>
2017-01-05 12:41:27 +0000

    palavras

--- src/forca/Palavras.java
+++ src/forca/Palavras.java
@@ -1,9 +1,13 @@
 package forca;
 
-import java.util.Random;
-
 public class Palavras {
 
-	
+	public String palavras() {
+		String palavras[] = {
+                "one", "tiger", "three", "arroz", "esdruxulo", "yolo", "bacon", "estoiro", "peperoni", "mascote"};
+            String palavra = palavras[(int) (Math.random() * palavras.length)];
+            
+            return palavra;
+	}
 
 }
